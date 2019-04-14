01c42c6fa382589a3837028be5ed6729e5343e10
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-04-14 13:17:17 +0430

    1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8100);
+server.listen(3001);
 
 app.get('/', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
